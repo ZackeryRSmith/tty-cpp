c6270439a4747788c21b8fd92b870759cfef7270
ZackeryRSmith <72983221+ZackeryRSmith@users.noreply.github.com>
2023-03-18 07:51:27 -0400

    Now recursivly scans the source

--- amalgamate.sh
+++ amalgamate.sh
@@ -2,7 +2,7 @@
 # Uses Heady to generate a single header file
 # :WARNING: ASSUMES A HEADY BINARY NAMED "heady" IS IN THE ROOT DIRECTORY! :WARNING:
 
-if !( ./heady --source src --inline "heady_t" --output "include/tty-cpp.hpp" ) ; then
+if !( ./heady --recursive --source src --output "include/tty-cpp.hpp" ) ; then
     echo "Heady failed, please make sure a compiled Heady binary is in your current directory!"
 fi
 
