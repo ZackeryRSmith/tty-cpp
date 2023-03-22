408f95be2370e1be0d66bf3a6a6f6f3c05927b7a
ZackeryRSmith <72983221+ZackeryRSmith@users.noreply.github.com>
2023-03-22 11:33:37 -0400

    Attempted to add BS and ESC detection

--- src/input.cpp
+++ src/input.cpp
@@ -1,27 +1,23 @@
-#include <iostream>
-#include <termios.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <cstdio>
-#include <csignal>
-
-#include "term.h"
-#include "input.h"
+#include "headers/term.h"
+#include "headers/input.h"
 
 Key Term::getkey() {
-    // disable raw mode on function return?
-    bool disable_raw = !is_raw_mode(); 
-    if (disable_raw) enable_raw_mode();
-    
     char c;
 
     if (read(STDIN_FILENO, &c, 1) == 1) {
         if (c == '\x1b') {
-            char seq[3];
+            char seq[4];
 
-            if (read(STDIN_FILENO, &seq[0], 1) == 1 && read(STDIN_FILENO, &seq[1], 1) == 1) {
-                if (disable_raw) disable_raw_mode();
-                if (seq[0] == '[') {
+            if (read(STDIN_FILENO, &seq[0], 1) != 1) return Key::ESC;
+            if (read(STDIN_FILENO, &seq[1], 1) != 1) return Key::ESC;
+
+            if (seq[0] == '[') {
+                if (seq[1] >= '0' && seq[1] <= '9') {
+                    if (read(STDIN_FILENO, &seq[2], 1) != 1) return Key::ESC;
+                    if (seq[2] == '~') {
+                        if (seq[1] == '3') return Key::DEL;
+                    }
+                } else {
                     switch (seq[1]) {
                         case 'A': return Key::UP_ARROW;
                         case 'B': return Key::DOWN_ARROW;
@@ -31,17 +27,15 @@ Key Term::getkey() {
                 }
             }
         } else if (c == '\r') {
-            if (disable_raw) disable_raw_mode();
             return Key::ENTER;
+        } else if (c == '\x7f') {
+            return Key::BS;
         } else if (c >= 1 && c <= 26) {
-            if (disable_raw) disable_raw_mode();
             return static_cast<Key>(static_cast<int>(Key::CTRL_A) + (c - 1));
         } else {
-            if (disable_raw) disable_raw_mode();
             return static_cast<Key>(c);
         }
     }
 
-    if (disable_raw) disable_raw_mode();
     return Key::UNKNOWN;
 }
