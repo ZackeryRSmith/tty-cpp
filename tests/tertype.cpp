c4ab48da93a97b07d61e444c9222eb71f2dae9e3
ZackeryRSmith <72983221+ZackeryRSmith@users.noreply.github.com>
2023-04-12 18:03:45 -0400

    Simple typeracer clone

--- tests/tertype.cpp
+++ tests/tertype.cpp
@@ -11,6 +11,71 @@
 
 #define VERSION "0.1"
 
+#define BGFILLCOLOR Term::ColorBit4::BLACK
+#define FGBORDERCOLOR Term::ColorBit4::WHITE
+#define CURRENTCHARCOLOR Term::ColorBit4::RED
+#define BEFORCHARCOLOR Term::ColorBit4::WHITE
+#define AFTERCHARCOLOR Term::ColorBit4::GRAY
+
+/**
+ * @brief Fills the screen with a given character and color.
+ *
+ * @param c The character to fill the screen with.
+ * @param color The color to use for filling the screen. Default value is no color.
+ *
+ * @details This function fills the entire screen with the specified character and color (if provided).
+ *
+ * Usage:
+ *  - To fill the screen with a character and color:
+ *      - fill_screen('x', Term::rgb(255, 0, 0)); // fills the screen with red 'x'
+ *      - fill_screen('-', Term::ColorBit4::Blue); // fills the screen with blue '-'
+ *  - To fill the screen with a character and no color:
+ *      - fill_screen('*'); // fills the screen with '*' in the default background color
+ *
+ */
+void fill_screen(char c, std::variant<std::monostate, Term::rgb, Term::ColorBit4> color = std::variant<std::monostate, Term::rgb, Term::ColorBit4>{}) {
+    Screen::clear();
+    Screen::Size term_size = Screen::size();
+    
+    std::string fill_color = "";
+
+    if (!std::holds_alternative<std::monostate>(color)) {
+        if (std::holds_alternative<Term::rgb>(color)) {
+            fill_color = Term::color_bg(std::get<Term::rgb>(color));
+        } else {
+            fill_color = Term::color_bg(std::get<Term::ColorBit4>(color));
+        }
+    }
+
+    for (int y = 0; y < term_size.columns; y++) {
+        for (int x = 0; x < term_size.rows; x++) {
+            std::cout << fill_color << c;
+        }
+    }
+
+    Cursor::home();
+}
+
+/**
+ * @brief Fills the screen with a given character and RGB color.
+ *
+ * @param c The character to fill the screen with.
+ * @param r The red component of the RGB color.
+ * @param g The green component of the RGB color.
+ * @param b The blue component of the RGB color.
+ *
+ * @details This function fills the entire screen with the specified character and RGB color.
+ *
+ * Usage:
+ *  - To fill the screen with a character and RGB color:
+ *      - fill_screen('x', 255, 0, 0); // fills the screen with red 'x'
+ *      - fill_screen('-', 0, 0, 255); // fills the screen with blue '-'
+ *
+ */
+void fill_screen(char c, size_t r, size_t g, size_t b) { 
+    fill_screen(c, Term::rgb(r, g, b)); 
+}
+
 std::string repeat_str(const std::string& str, const size_t n) {
     std::string result;
     result.reserve(n * str.length());
@@ -79,6 +144,7 @@ class Typer {
             
             Cursor::set(round(term_size.rows / 2) - 2, 0);
 
+            std::cout << Term::color_fg(FGBORDERCOLOR) << Term::color_bg(BGFILLCOLOR);
             std::cout << repeat_str("─", round(term_size.columns / 2)) << "┬" << repeat_str("─", round(term_size.columns / 2)) << std::endl;
             Cursor::next_line();
             std::cout << repeat_str("─", round(term_size.columns / 2)) << "┴" << repeat_str("─", round(term_size.columns / 2));
@@ -86,19 +152,21 @@ class Typer {
 
             std::cout << repeat_str(" ", round(term_size.columns / 2) - ((pointer) - spos)) // space before text 
                       /* TEXT BEFORE CHAR */
+                      << Term::color_fg(BEFORCHARCOLOR)
                       << text.substr(spos, (pointer) - spos)
+                      << Term::style(Term::Style::RESET)
                       /********************/
 
                       /* ON CHAR */
-                      << (text.at(pointer) == ' ' ? Term::color_bg(Term::ColorBit4::RED) : Term::color_fg(Term::ColorBit4::RED)) 
+                      << (text.at(pointer) == ' ' ? Term::color_bg(CURRENTCHARCOLOR) : Term::color_fg(CURRENTCHARCOLOR)) 
                       << Term::style(Term::Style::BOLD)
                       << text.at(pointer)
                       << Term::style(Term::Style::RESET)
                       /***********/
 
                       /* TEXT AFTER CHAR */
-                      << Term::color_fg(Term::ColorBit4::GRAY) 
-                      << text.substr(pointer + 1, round(term_size.columns / 2) - 1)
+                      << Term::color_fg(AFTERCHARCOLOR) 
+                      << text.substr(pointer + 1, round(term_size.columns / 2))
                       /*******************/
 
                       << repeat_str(" ", round(term_size.columns / 2) - (text.size() - (pointer))) // space after text
@@ -147,6 +215,8 @@ int main() {
     RawModeGuard raw_mode_guard;
     Cursor::hide();
     Screen::clear();
+
+    fill_screen(' ', BGFILLCOLOR);
 
     std::cout << Term::style(Term::Style::BOLD) << "TerType " << Term::style(Term::Style::RESET) << "- V" << VERSION;
 
